72436f765842c700b6eb143e7203d84e50522db6
Chaw Chit Su Thwe <61979480+chawchitsuthwe@users.noreply.github.com>
2020-08-27 18:44:54 +0630

    Add new route

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 import Nav from './components/Nav';
+import Footer from './components/Footer';
 import Home from './components/Home';
+import Result from './components/Result';
 
 const App = () => {
   return (
@@ -11,8 +13,10 @@ const App = () => {
     <Router>
       <Switch>
               <Route exact path="/" component={Home}/>
+              <Route exact path="/result" component={Result}/>
           </Switch>
     </Router>
+    <Footer />
   </div>
 );
 }
