f6938357ce98a5fdf8ba80ab95036a184cc5c7d0
Chaw Chit Su Thwe <61979480+chawchitsuthwe@users.noreply.github.com>
2020-08-27 18:46:21 +0630

    Add Category Card

---
+++ src/components/CategoryCard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './CategoryCard.css'
+
+const CategoryCard = () => {
+	return (
+		<div className="d-inline-flex m-1 m-md-2 m-lg-3 justify-content-center align-items-center card p-4">
+			<h4 className="text-secondary">Category</h4>
+		</div>
+	)
+}
+
+export default CategoryCard
