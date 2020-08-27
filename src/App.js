84e9a75a5f9671085f6d5db6ce47a6921085ae76
Chaw Chit Su Thwe <61979480+chawchitsuthwe@users.noreply.github.com>
2020-08-27 21:01:34 +0630

    Updated

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Nav from './components/Nav';
 import Footer from './components/Footer';
 import Home from './components/Home';
+import JobDetail from './components/JobDetail';
 import Result from './components/Result';
 
 const App = () => {
@@ -12,8 +13,9 @@ const App = () => {
     <Nav />
     <Router>
       <Switch>
-              <Route exact path="/" component={Home}/>
-              <Route exact path="/result" component={Result}/>
+            <Route exact path="/" component={Home}/>
+            <Route exact path="/result" component={Result}/>
+            <Route exact path="/job" component={JobDetail}/>
           </Switch>
     </Router>
     <Footer />
