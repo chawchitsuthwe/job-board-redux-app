55db9d2afd70a6d081a62415e7e370dc255839fd
Chaw Chit Su Thwe <61979480+chawchitsuthwe@users.noreply.github.com>
2020-08-27 21:02:16 +0630

    Update

--- src/components/CategoryCard.js
+++ src/components/CategoryCard.js
@@ -1,7 +1,5 @@
 import React from 'react';
 
-import './CategoryCard.css'
-
 const CategoryCard = () => {
 	return (
 		<div className="d-inline-flex m-1 m-md-2 m-lg-3 justify-content-center align-items-center card p-4">
