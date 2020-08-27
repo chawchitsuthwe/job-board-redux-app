7c7e20ef564aedaaa30b45193e2748f149fcb14f
Chaw Chit Su Thwe <61979480+chawchitsuthwe@users.noreply.github.com>
2020-08-27 18:48:05 +0630

    Add NoResult Component

---
+++ src/components/NoResult.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const NoResult = () => {
+	return (
+		<div>
+			<h1>Nothing Found</h1>
+			<p>Sorry, but nothing matched your search of <strong>[user search keywords]</strong>. <br />
+				Please try again with some different keywords.</p>
+	
+		</div>
+	)
+}
+
+export default NoResult
