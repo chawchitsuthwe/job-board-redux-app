683abd362d6403ed4b643287b432befbdd69fc99
Chaw Chit Su Thwe <61979480+chawchitsuthwe@users.noreply.github.com>
2020-08-28 18:21:57 +0630

    Add Tag component

---
+++ src/components/Tag.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Tag.css';
+
+const Tag = () => {
+	return (
+		<div className="tag-card d-inline-flex bg-dark">
+			<p>Tag</p>
+		</div>
+	)
+}
+
+export default Tag
